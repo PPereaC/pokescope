3690e0a3a58a9d520626fc8b930749c478cb7492
Pablo Perea Campos <pablopereac02@gmail.com>
2025-12-13 19:58:23 +0100

    add: Implementación de búsqueda de pokemons

--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -5,11 +5,15 @@ import PokeTarjeta from "../components/PokeTarjeta"
 
 const Index = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [listado, setListado] = useState([]);
+  const [filtro, setFiltro] = useState('');
   const [offset, setOffset] = useState(0);
   const [limite, setLimite] = useState(20);
 
   useEffect(() => {
-    getPokemons(offset);
+    getPokemons(offset)
+    getAllPokemons()
   },[])
 
   const getPokemons = async(o) => {
@@ -17,7 +21,33 @@ const Index = () => {
     axios.get(url).then(async(response) => {
       const respuesta = response.data;
       setPokemons(respuesta.results);
+      setListado(respuesta.results);
     })
+  }
+
+  const getAllPokemons = async(o) => {
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
+    axios.get(url).then(async(response) => {
+      const respuesta = response.data;
+      setPokemons(respuesta.results);
+      setAllPokemons(respuesta.results);
+    })
+  }
+
+  const buscar = async(e) => {
+    if(e.keyCode == 13) {
+      if(filtro.trim() != '') {
+        setListado([]);
+        setTimeout(() => {
+          setListado(allPokemons.filter(p => p.name.includes(filtro)))
+        },100)
+      } else if(filtro.trim() == '') {
+        setListado([]);
+        setTimeout(() => {
+          setListado(pokemons)
+        },100)
+      }
+    }
   }
 
   return (
@@ -27,12 +57,14 @@ const Index = () => {
           <Col>
             <InputGroup className="mt-3 mb-3 shadow">
               <InputGroupText><i className="fa-solid fa-search"></i></InputGroupText>
-              <Input placeholder="Buscar Pokemon"></Input>
+              <Input value={filtro} onChange={(e) => {setFiltro(e.target.value)}}
+                onKeyUpCapture={buscar}
+                placeholder="Buscar Pokemon"></Input>
             </InputGroup>
           </Col>
         </Row>
         <Row className="mt-3">
-          { pokemons.map( (pok, i) => (
+          { listado.map( (pok, i) => (
             <PokeTarjeta poke={pok} key={i}></PokeTarjeta>
           )) }
         </Row>
