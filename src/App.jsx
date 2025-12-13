558e1e812e936a510f3767564516e48427b71304
Pablo Perea Campos <pablopereac02@gmail.com>
2025-12-13 18:11:52 +0100

    add: Rutas en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import index from './views'
+import Detalle from './views/Detalle'
 
 function App() {
 
   return (
     <>
       
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<index />} />
+          <Route path='/pokemon/:id' element={<Detalle />} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
